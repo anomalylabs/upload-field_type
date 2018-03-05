3cd07aba7562df23f3bb426dc78aae3d8610ad82
Aranea Development <info@aranea-development.nl>
2018-03-05 10:36:31 +0100

    Fixed php.ini size detection.

--- src/UploadFieldType.php
+++ src/UploadFieldType.php
@@ -124,8 +124,8 @@ class UploadFieldType extends FieldType
     {
         $config = parent::getConfig();
 
-        $post = str_replace('M', '', ini_get('post_max_size'));
-        $file = str_replace('M', '', ini_get('upload_max_filesize'));
+        $post = $this->getSize('post_max_size') ;
+        $file = $this->getSize('upload_max_filesize');
 
         $server = $file > $post ? $post : $file;
 
@@ -201,5 +201,17 @@ class UploadFieldType extends FieldType
     public function getColumnName()
     {
         return parent::getColumnName() . '_id';
+    }
+
+    /**
+     * Get php.ini value and convert it to Mb if needed.
+     *
+     * @param $key
+     * @return float|int
+     */
+    public function getSize($key)
+    {
+        preg_match('/([0-9]*)(M|G)/im', ini_get($key), $matches);
+        return $matches[2] === 'G' ? 1024 * $matches[1] : $matches[1];
     }
 }
